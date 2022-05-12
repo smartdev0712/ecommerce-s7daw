84e18de55ae1492cba66b3246eafee7a28ac34eb
artem712 <artemlarch77@gmail.com>
2022-05-12 07:39:40 -0700

    change backend url

--- components/http.js
+++ components/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:1337",
+    baseURL: process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337",
     headers: {
         "Content-type": "application/json",
     },

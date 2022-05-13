e1a784f8a7b2541eb20270ced32cd6c5b896b0ca
artem712 <artemlarch77@gmail.com>
2022-05-13 05:16:17 -0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL,
+  },
   reactStrictMode: true,
   images: {
     loader: "default",
-    domains: ["localhost", "strapi-floor-metaverse-ww5pg.ondigitalocean.app"],
+    domains: [
+      "localhost",
+      "strapi-floor-metaverse-ww5pg.ondigitalocean.app",
+      "cloudinary.com",
+      "res.cloudinary.com",
+    ],
   }
 }
 

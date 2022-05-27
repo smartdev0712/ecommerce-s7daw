23ea193c799f0531f72def5fdf7e32e8dd4bddf9
artem712 <artemlarch77@gmail.com>
2022-05-26 17:52:15 -0700

    change api token and backend url

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@
 
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: "https://floors-app-lz6o6.ondigitalocean.app",
+    NEXT_PUBLIC_API_URL: "https://meta-floor-strapi-app.herokuapp.com",
     GOOGLE_MAP_API_KEY:"AIzaSyBpK2K9D5QfMMiTVdsgz9cSjoKcXmhRsOo",
-    FULL_ACCESS_TOKEN:"95181fce83979d2bc7254bd60664bb96a0f23c0cc334b3233a726d0a8b2f983fa826a50a4e3c1dcfd44446bbb45f460cfab60f4bc8e30c3c6ef50d08d5836c18e64c5765d13bbdef1bdaf2829f9f6407ca44ec7ceae49c5efa388148511d4d2ebeb50278823f3cfbdfa5cc3cd12cfa6f0b1c7c1ee5c3341b86326448781f64a5",
-    READ_ONLY_TOKEN:"f30b2605047a0f61a84fdad6e39f510ba8eae88bc9c625dddfde69853eaeb1d54d8042daff6a9abef564f7d361e2bf954ebd976ebaf43a57e146d71a869ded0bd01097034d3cf921874e61bb79bbcf26798b034beb14ddde6da39ed362684dac15f0865a449145da24e01b8e53b00eae1d7a5c508c8bf2d5c25e8efc8aa8e35f",
+    FULL_ACCESS_TOKEN:"065fd41ce83bf1656e6e45f74bbd301ab893e73368727ca35e296a8dcc71d37b9a9e4662d10d56380d5dcd1b26cf792d8e1d04ad97af3f86e5fd74fafa05e6e729eb6e3ee11bdcd3a7435555b7039d36e7672a7c0df82b7a7448878f1ff8109964b43e28abeab6a764b3ba1df3f88f2afe5baf1e6026e0ad827c59001305c5e2",
+    READ_ONLY_TOKEN:"3e513f77fe642a2add2ad30ed4cf42651e3fd1300cd9329aba718b66e0e400e8d8daf00467133f4f17697bd889d3e0e0d29b46e44939bf0936e9b9d194b3dc8f69221f06489aea245083bbde1897ebd584512598be2661b2b6f3d41955f3ace8eb1750e0f76e176bb6ea4ea2dfbf277a7384bc308f1121f4ed43ac206bb42f86",
   },
   reactStrictMode: true,
   images: {
     loader: "default",
     domains: [
       "localhost",
-      "floors-app-lz6o6.ondigitalocean.app",
+      "meta-floor-strapi-app.herokuapp.com",
       "cloudinary.com",
       "res.cloudinary.com",
     ],
